813a21482aa874513178e10283ae0ee0670a3650
Ramón García Fernández <ramon.garcia.f@gmail.com>
2017-04-23 02:06:30 +0200

    Javascript functionality competing

--- libbeat/processors/actions/javascript.go
+++ libbeat/processors/actions/javascript.go
@@ -10,8 +10,9 @@ import (
 )
 
 type javascript struct {
-	vm     *otto.Otto
-	script string
+	vm             *otto.Otto
+	scriptCompiled *otto.Script
+	script         string
 }
 
 func init() {
@@ -22,15 +23,38 @@ func init() {
 }
 
 func newJavascript(c common.Config) (processors.Processor, error) {
-	type config struct {
+	var config struct {
 		script string `config:"script"`
 	}
-
-	var myconfig config
-	err := c.Unpack(&myconfig)
+	err := c.Unpack(&config)
 	if err != nil {
-		logp.Warn("Error unpacking config for grok")
-		return nil, fmt.Errorf("fail to unpack the grok configuration: %s", err)
+		logp.Warn("Error unpacking config for javascript")
+		return nil, fmt.Errorf("fail to unpack the javascript configuration: %s", err)
 	}
+	vm := otto.New()
+	scriptCompiled, err := vm.Compile("", config.script)
+	if err != nil {
+		logp.Warn("Error compiling javascript script")
+		return nil, fmt.Errorf("compiling javascript script: %s", err)
+	}
+	return javascript{vm, scriptCompiled, config.script}, nil
+}
 
+func (j javascript) Run(event common.MapStr) (common.MapStr, error) {
+	object, _ := j.vm.Object(`{}`)
+	for k, v := range event {
+		object.Set(k, v)
+	}
+	j.vm.Set("event", object)
+	j.vm.Run(j.script)
+	result := common.MapStr{}
+	for _, k := range object.Keys() {
+		value, _ := object.Get(k)
+		result[k], _ = value.ToString()
+	}
+	return result, nil
+}
+
+func (j javascript) String() string {
+	return "javascript script=<![CDATA[" + j.script + "]]>"
 }
